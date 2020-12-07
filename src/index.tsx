0a7c78307ae24a96bdc5a3d9e6421a3fde0b5bc4
Gia Huy <nguyengiahuy16@gmail.com>
2020-12-07 15:26:38 +0700

    start ok

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.scss';
 
 function App() {
-  return <div>Hello</div>;
+  return <div>Hello 123</div>;
 }
+
+ReactDOM.render(<App />, document.getElementById('yuhRoot'));
+
+
