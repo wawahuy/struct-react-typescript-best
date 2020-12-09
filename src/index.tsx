e1beab901b2afdcd82b826853ee66373bbea1639
Gia Huy <nguyengiahuy16@gmail.com>
2020-12-09 22:26:45 +0700

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.scss"
 
-import logo from './assets/images/a.png';
-import tff from './assets/fonts/a.ttf';
+import logo from "./assets/images/a.png"
+import tff from "./assets/fonts/a.ttf"
 
 function App() {
-  return <div><img src={logo}></img></div>;
+  return (
+    <div>
+      <img src={logo}></img>
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('yuhRoot'));
-
-
+ReactDOM.render(<App />, document.getElementById("yuhRoot"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-const devMode = process.env.NODE_ENV !== 'production';
+const devMode = process.env.NODE_ENV !== "production"
 
 const config = {
   entry: {
-    test: './src/index.tsx'
+    test: "./src/index.tsx"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve("src"),
+      "@@": path.resolve()
+    }
   },
   module: {
     rules: [
@@ -21,60 +28,63 @@ const config = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
               sourceMap: true,
               importLoaders: 1,
               modules: {
-                localIdentName: '[local]___[hash:base64:5]'
+                localIdentName: "[local]___[hash:base64:5]"
               }
             }
           },
-          'sass-loader',
+          "sass-loader"
         ]
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
       },
       {
         test: /\.(jpg|png|gif|svg)$/,
         use: [
-        {
-            loader: 'file-loader',
+          {
+            loader: "file-loader",
             options: {
-                name: '[name].[ext]',
-                outputPath: './assets/images',
+              name: "[name].[ext]",
+              outputPath: "./assets/images"
             }
-        }]
+          }
+        ]
       },
       {
         test: /\.(ttf|woff|jfproj)$/,
         use: [
-        {
-            loader: 'file-loader',
+          {
+            loader: "file-loader",
             options: {
-                name: '[name].[ext]',
-                outputPath: './assets/fonts',
+              name: "[name].[ext]",
+              outputPath: "./assets/fonts"
             }
-        }]
-      },
+          }
+        ]
+      }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: path.join(__dirname, "/public/index.html") }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "/public/index.html")
+    }),
     new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
     })
   ],
   devServer: {
     hot: true,
-    port: 3000,
-  },
+    port: 3000
+  }
 }
 
-
-module.exports = config;
+module.exports = config
